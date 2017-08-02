34618a5fd82f63c571cec234ccee247ca1dda975
ellenwelsh92 <ellenwelsh@Ellens-iMac-2.local>
2017-08-02 14:00:05 +0100

    Updated featured posts css

--- components/blog/featuredPosts/featured.php
+++ components/blog/featuredPosts/featured.php
@@ -1,56 +1,55 @@
-<div id="featuredSection" class="pure-g">
-
-<div class="alignCenter pure-u-1">
-
-            <img src="../../../assets/images/pigtail_grey.svg" alt="pig-tale" />
-
-            <h2 id="featuredPostsTitle">Featured Posts</h2>
-
-
+<section id="featured-blog">
   <div class="container pure-g">
-                <!-- Image -->
-  <div class="pure-u-1 pure-u-sm-1-4">
-                <img class="pure-img" src="../../../assets/images/blogImages/iStock-476191579.jpg" alt="pig-tale" />
-              <div id="whiteBlogBg">
-                <h3 id="marginReduce">Lorem ipsum dolor sit amet</h3>
-                <button id="readMoreBtn"  class="fontCapital">Read More</button>
-              </div>
 
-      </div> <!-- pure-u-1 pure-u-sm-1-4 -->
-
-                <!--Image -->
-  <div class="pure-u-1 pure-u-sm-1-4">
-                <img class="pure-img" src="../../../assets/images/blogImages/iStock-476191579.jpg" alt="pig-tale" />
-
-                <h3>Lorem ipsum dolor sit amet</h3>
-                <button id="readMoreBtn"  class="fontCapital">Read More</button>
-
-      </div> <!-- pure-u-1 pure-u-sm-1-4 -->
-
-                <!-- Image -->
-  <div class="pure-u-1 pure-u-sm-1-4">
-                <img class="pure-img" src="../../../assets/images/blogImages/iStock-476191579.jpg" alt="pig-tale" />
-
-                <h3>Lorem ipsum dolor sit amet</h3>
-                <button id="readMoreBtn"  class="fontCapital">Read More</button>
-
-        </div> <!-- pure-u-1 pure-u-sm-1-4 -->
-
-  <div class="pure-u-1 pure-u-sm-1-4">
-               <img class="pure-img" src="../../../assets/images/blogImages/iStock-476191579.jpg" alt="pig-tale" />
-
-               <h3>Lorem ipsum dolor sit amet</h3>
-               <button id="readMoreBtn"  class="fontCapital">Read More</button>
-
-          </div> <!-- pure-u-1 pure-u-sm-1-4 -->
+    <div class="pure-u-1">
+          <img class="pure-img piggy-tale-title-logo" src="../../../assets/images/pigtail_grey.svg" alt="pig-tale" />
+          <h2>Featured Posts</h2>
+    </div> <!-- .pure-u-1 -->
 
 
-      </div> <!-- container pure-g -->
+    <div class="container pure-g">
+                  <!-- Image -->
+    <div class="pure-u-1 pure-u-sm-1-4">
+                  <img class="pure-img" src="../../../assets/images/blogImages/iStock-476191579.jpg" alt="pig-tale" />
+                <div class="featured-blog-text">
+                      <h3>Featured Post 1</h3>
+                      <button>Read More</button>
+                </div>
+    </div> <!-- pure-u-1 pure-u-sm-1-4 -->
+
+                  <!-- Image -->
+    <div class="pure-u-1 pure-u-sm-1-4">
+                  <img class="pure-img" src="../../../assets/images/blogImages/iStock-476191579.jpg" alt="pig-tale" />
+                <div class="featured-blog-text">
+                      <h3>Featured Post 2</h3>
+                      <button>Read More</button>
+                </div>
+    </div> <!-- pure-u-1 pure-u-sm-1-4 -->
+
+                  <!-- Image -->
+    <div class="pure-u-1 pure-u-sm-1-4">
+                  <img class="pure-img" src="../../../assets/images/blogImages/iStock-476191579.jpg" alt="pig-tale" />
+                <div class="featured-blog-text">
+                      <h3>Featured Post 3</h3>
+                      <button>Read More</button>
+                </div>
+    </div> <!-- pure-u-1 pure-u-sm-1-4 -->
+
+                  <!-- Image -->
+    <div class="pure-u-1 pure-u-sm-1-4">
+                  <img class="pure-img" src="../../../assets/images/blogImages/iStock-476191579.jpg" alt="pig-tale" />
+                <div class="featured-blog-text">
+                      <h3>Featured Post 4</h3>
+                      <button>Read More</button>
+                </div>
+    </div> <!-- pure-u-1 pure-u-sm-1-4 -->
 
 
 
 
-        </div> <!-- alignCenter -->
+        </div> <!-- container pure-g -->
 
 
-</div> <!-- featuredSection -->
+
+  </div>
+</section>
