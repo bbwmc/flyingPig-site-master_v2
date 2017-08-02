e5b4e6c217d0233d36d2ace4a8546bc0dc17b6e1
ellenwelsh92 <ellenwelsh@Ellens-iMac-2.local>
2017-08-02 13:26:48 +0100

    Updated css for gallery and ratings

--- components/venue/ratings/ratings.php
+++ components/venue/ratings/ratings.php
@@ -1,44 +1,47 @@
-<div id="ratings" class="desktop-hide">
+<section id="piggy-ratings"  class="desktop-hide">
+  <div class="container pure-g">
 
-  <div id="aboutOpts" class="alignCenter">
 
-    <div id="ratingsBg">
-      <h2 id="ratingsTitle" class="alignCenter"> What Other <br> Piggies Say</h2>
+        <div class="ratings-Bg pure-u-1 pure-u-sm-2-4">
+          <h2 class="ratingsTitle"> What Other <br> Piggies Say</h2>
 
-    <!-- Percent -->
-    <div>
-        <p id="percentageSize">
-        <img  src="../../../assets/images/dialogue_icon.svg" alt="pig-tale" /> 85%</p>
-    </div>
+        <!-- Percent -->
+        <div>
+            <p class="percentageSize">
+            <img src="../../../assets/images/dialogue_icon.svg" alt="pig-tale" style="margin-bottom: 8px;" /> 85%</p>
+        </div>
 
-    <div>
-        <p id="averageRating"><span style="font-weight: bold">Average Rating</span>
-        <br>From <span style="font-weight: bold">7321</span> Piggies' Reviews</p>
-    </div>
+        <div>
+            <p class="averageRating"><span style="font-weight: bold">Average Rating</span>
+            <br>From <span style="font-weight: bold">7321</span> Piggies' Reviews</p>
+        </div>
 
-    <div id="ratingInfo2" class="pure-g">
+        <div class="rating-Info pure-g">
 
-    <!-- <div class="pure-u-1-4"><img class="aboutImg" src="http://via.placeholder.com/42x61" alt="about" /></div> -->
+        <div class="textRight1 pure-u-1-2">
 
-    <div id="textRight1" class="pure-u-1-2">
+                <p><span>88%</span> Location</p>
 
-          <p><span>88%</span> Location</p>
+                <p><span>90%</span> Staff</p>
 
-          <p><span>90%</span> Staff</p>
+                <p><span>88%</span> Fun</p>
+          </div> <!-- textRight -->
 
-          <p><span>88%</span> Fun</p>
-    </div> <!-- textRight -->
+          <div class="textRight2 pure-u-1-2">
 
-    <div id="textRight2" class="pure-u-1-2">
+                <p><span>88%</span> Safety</p>
 
-          <p><span>88%</span> Safety</p>
+                <p><span>90%</span> Facilities</p>
 
-          <p><span>90%</span> Facilities</p>
+                <p><span>88%</span> Cleanliness</p>
 
-          <p><span>88%</span> Cleanliness</p>
+            </div> <!-- textRight -->
+          </div> <!-- ratingInfo -->
+        </div> <!-- ratingsBg -->
 
-        </div> <!-- textRight -->
-      </div> <!-- ratingInfo -->
-    </div> <!-- ratingsBg -->
-  </div> <!-- aboutOpts -->
-</div> <!-- ratings -->
+
+
+  </div>
+
+
+</section>
